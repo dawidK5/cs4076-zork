3df3865144e30ff0a2d3ef43144d6f89ad2f4615
Elliothc13 <55023402+Elliothc13@users.noreply.github.com>
2021-03-19 11:35:22 +0000

    Add files via upload

---
+++ CMDmap.cpp
@@ -0,0 +1,22 @@
+else if (commandWord.compare("map") == 0)
+		{
+        cout << "         [b2] --- [c2] --- [d2] --- [e2] --- [f2]         " << endl;
+        cout << "          |        |        |        |        |           " << endl;
+        cout << "          |        |        |        |        |           " << endl;
+        cout << "         [b3] --- [c3] --- [d3] --- [e3] --- [f3]         " << endl;
+        cout << "          |        |        |        |        |           " << endl;
+        cout << "          |        |        |        |        |           " << endl;
+        cout << "[a4] --- [b4] --- [c4] --- [d4] --- [e4] --- [f4] --- [g4]" << endl;
+        cout << " |        |                 |        |        |        |  " << endl;
+        cout << " |        |                 |        |        |        |  " << endl;
+        cout << "[a5] --- [b5]              [d5] --- [e5] --- [f5] --- [g5]" << endl;
+        cout << " |        |                 |        |        |        |  " << endl;
+        cout << " |        |                 |        |        |        |  " << endl;
+        cout << "[a6] --- [b6] --- [c6] --- [d6] --- [e6] --- [f6] --- [g6]" << endl;
+        cout << " |        |        |        |        |        |        |  " << endl;
+        cout << " |        |        |        |        |        |        |  " << endl;
+        cout << "[a7] --- [b7] --- [c7] --- [d7] --- [e7] --- [f7] --- [g7]" << endl;
+        cout << "          |        |        |        |        |        |  " << endl;
+        cout << "          |        |        |        |        |        |  " << endl;
+        cout << "         [b8] --- [c8] --- [d8] --- [e8] --- [f8] --- [g8]" << endl;
+		}
